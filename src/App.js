dfa640d269ef21e442e9058839fd278183759770
Binh Nguyen <b.nguyen.sn@gmail.com>
2019-09-24 00:18:22 +0100

    Update general layout to use JSS

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from './components/layout/Header';
 import Body from './components/layout/Body';
 import Footer from './components/layout/Footer';
+import Background from './components/layout/Background';
 import './app.css';
 
 export default function App() {
   return (
     <div className="app">
+      <Background />
       <Header />
       <Body />
       <Footer />
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,49 @@
 import React from 'react';
-import './header.css';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+  header: {
+    padding: '2rem',
+
+    color: '#ffffff',
+    '& h1': {
+      display: 'inline-block',
+    },
+    '& a': {
+      color: '#ffffff',
+      transition: 'color 0.33s',
+    },
+  },
+  nav: {
+    width: '100%',
+    '& ul': {
+      display: 'flex',
+      flexDirection: 'row-reverse',
+      alignItems: 'center',
+    },
+    '& li:first-child': {
+      flexGrow: 1,
+      textAlign: 'right',
+    },
+    '& li:not(:first-child)': {
+      margin: '0 1rem',
+    },
+  },
+  divider: {
+    margin: '20px auto 0 auto',
+    width: '80%',
+    height: 2,
+    background: '#eeeeee',
+    opacity: 0.5,
+  },
+});
 
 export default function Header() {
+  const classes = useStyles();
+
   return (
-    <div className="header">
-      <nav>
+    <div className={classes.header}>
+      <nav className={classes.nav}>
         <ul>
           <li>
             <a href="/" aria-label="Druid Street Labs' homepage">
@@ -23,6 +62,7 @@ export default function Header() {
           </li>
         </ul>
       </nav>
+      <div className={classes.divider} />
     </div>
   );
 }
