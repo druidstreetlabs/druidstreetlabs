a32d39dd29201e3e105774582da6f5282ecf3aff
Binh Nguyen <b.nguyen.sn@gmail.com>
2019-09-26 11:45:15 +0100

    Fix horizontal scrollbar in the Background component

--- src/components/layout/Background.js
+++ src/components/layout/Background.js
@@ -6,19 +6,16 @@ import Circle from '../shapes/Circle';
 
 const useStyles = createUseStyles({
   background: {
-    position: 'fixed',
+    position: 'absolute',
     top: 0,
     left: 0,
     width: '100vw',
+    maxWidth: '100%',
     height: '100vh',
     background:
       'linear-gradient(45deg, rgba(124,89,192,1) 0%, rgba(92,108,192,1) 25%, rgba(77,152,181,1) 65%)',
     zIndex: -1,
-  },
-  canvas: {
-    position: 'relative',
-    width: '100vw',
-    height: '100vh',
+    overflow: 'hidden',
   },
 });
 
@@ -27,72 +24,70 @@ export default function Background() {
 
   return (
     <div className={classes.background}>
-      <div className={classes.canvas}>
-        {/*<BackgroundDivider color="linear-gradient(45deg, rgba(124,89,192,1) 0%, rgba(92,108,192,1) 50%, rgba(77,152,181,1) 100%)" />*/}
-        <BackgroundCurtain />
+      {/*<BackgroundDivider color="linear-gradient(45deg, rgba(124,89,192,1) 0%, rgba(92,108,192,1) 50%, rgba(77,152,181,1) 100%)" />*/}
+      <BackgroundCurtain />
 
-        <Circle
-          radius={1000}
-          styles={{
-            top: -500,
-            right: -500,
-            borderTop: '5px solid #5c6cc0',
-            borderBottom: '5px solid #5c6cc0',
-            opacity: 0.5,
-          }}
-        />
+      <Circle
+        radius={1000}
+        styles={{
+          top: -500,
+          right: -500,
+          borderTop: '5px solid #5c6cc0',
+          borderBottom: '5px solid #5c6cc0',
+          opacity: 0.5,
+        }}
+      />
 
-        <Hexagon
-          top={100}
-          left={210}
-          width={50}
-          mainColor="#21359D"
-          topColor="#21359D"
-          bottomColor="#21359D"
-        />
+      <Hexagon
+        top={100}
+        left={210}
+        width={50}
+        mainColor="#21359D"
+        topColor="#21359D"
+        bottomColor="#21359D"
+      />
 
-        <Hexagon
-          top={100}
-          left={100}
-          width={50}
-          mainColor="#481D9C"
-          topColor="#481D9C"
-          bottomColor="#481D9C"
-        />
-        <Hexagon
-          top={195}
-          left={155}
-          width={50}
-          mainColor="#146E90"
-          topColor="#146E90"
-          bottomColor="#146E90"
-        />
+      <Hexagon
+        top={100}
+        left={100}
+        width={50}
+        mainColor="#481D9C"
+        topColor="#481D9C"
+        bottomColor="#481D9C"
+      />
+      <Hexagon
+        top={195}
+        left={155}
+        width={50}
+        mainColor="#146E90"
+        topColor="#146E90"
+        bottomColor="#146E90"
+      />
 
-        <Hexagon
-          top={100}
-          left={-10}
-          width={50}
-          mainColor="#307E9C"
-          topColor="#307E9C"
-          bottomColor="#307E9C"
-        />
-        <Hexagon
-          top={195}
-          left={45}
-          width={50}
-          mainColor="613CAA"
-          topColor="613CAA"
-          bottomColor="613CAA"
-        />
-        <Hexagon
-          top={290}
-          left={100}
-          width={50}
-          mainColor="9D7FD7"
-          topColor="9D7FD7"
-          bottomColor="9D7FD7"
-        />
-      </div>
+      <Hexagon
+        top={100}
+        left={-10}
+        width={50}
+        mainColor="#307E9C"
+        topColor="#307E9C"
+        bottomColor="#307E9C"
+      />
+      <Hexagon
+        top={195}
+        left={45}
+        width={50}
+        mainColor="613CAA"
+        topColor="613CAA"
+        bottomColor="613CAA"
+      />
+      <Hexagon
+        top={290}
+        left={100}
+        width={50}
+        mainColor="9D7FD7"
+        topColor="9D7FD7"
+        bottomColor="9D7FD7"
+      />
     </div>
   );
 }
